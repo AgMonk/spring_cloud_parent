b167871ca28654c5e5876d4bee1ccca9768cc76d
ginstone <hjg719@139.com>
2022-06-27 14:41:45 +0800

    Initial commit

---
+++ parent/cloud_01_eureka_server/src/main/java/com/gin/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2022/6/25 11:00
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
---
+++ parent/cloud_02_eureka_client/src/main/java/com/gin/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2022/6/25 16:55
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
