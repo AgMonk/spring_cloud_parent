52879d9e530cff16749dc34dab1828d7c926d1e4
ginstone <hjg719@139.com>
2022-06-27 15:53:02 +0800

    Consul 客户端 - 依赖

---
+++ parent/cloud_03_consul_client/src/main/java/com/gin/ConsulClientApplication.java
@@ -0,0 +1,19 @@
+package com.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2022/6/27 15:34
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClientApplication.class, args);
+    }
+
+}
