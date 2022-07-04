14a2d05db7c8e9fbf3e208f9239b483420c06cce
ginstone <hjg719@139.com>
2022-07-04 15:43:06 +0800

    config server - 创建

---
+++ parent/cloud_12_config_server/src/main/java/com/gin/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2022/7/4 15:31
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
