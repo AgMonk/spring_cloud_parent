9b315a76ed04f845403961b6d2f8ac880d514f9a
ginstone <hjg719@139.com>
2022-07-04 10:22:08 +0800

    Gateway组件 - 配置网关组建

---
+++ parent/cloud_11_gateway/src/main/java/com/gin/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2022/7/4 10:07
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
